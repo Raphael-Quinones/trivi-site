1c7ff596efc795b355421cc4e4d03c6ca2a6c32a
Raphael Quinones <Raphael Quinones>
2022-08-26 15:36:39 +0800

    put sendAPIRequest inside useEffect to avoid errors

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,25 +5,26 @@ export default function(){
     
     const [questions, setQuestions] = React.useState({})
     
-    //await only works inside async functions
-    async function sendAPIRequest(){
-        const response = await fetch('https://opentdb.com/api.php?amount=10',
-        { mode: "cors" });
-        const trivias = await response.json()
-        return trivias.results
-    }
+    
     
 
     
     //sendAPIRequest() executes twice when called
     //So I utilized useEffect to just call it once
     //works haha
-    React.useEffect(async ()=>{
-        const triviaArray = await sendAPIRequest()//this function is async, we need await to get results
-        //therefore anonymous function should also be async
-        setQuestions(triviaArray)
-        //I still don't know exactly why, but storing the array using hooks works
-        //maybe because after the whole page refreshes, questions variables are now semi-permanently changed
+    React.useEffect(()=>{
+        //await only works inside async functions
+        async function sendAPIRequest(){
+            const response = await fetch('https://opentdb.com/api.php?amount=10',
+            { mode: "cors" });
+            const trivias = await response.json()
+
+            setQuestions(trivias.results)
+            //I still don't know exactly why, but storing the array using hooks works
+            //maybe because after the whole page refreshes, questions variables are now semi-permanently changed
+        }
+        sendAPIRequest()
+;        
     },[])
     
     for (let categ in questions){
