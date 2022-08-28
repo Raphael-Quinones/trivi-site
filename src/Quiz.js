48faa13b84b1b075701191657b1afd7b62c443d6
Raphael Quinones <wapquinones28@gmail.com>
2022-08-28 22:15:01 +0800

    Trivias now only change once

--- src/Quiz.js
+++ src/Quiz.js
@@ -7,6 +7,7 @@ export default function(){
     
     const [questions, setQuestions] = React.useState({})
     const [playing, setPlaying] = React.useState(true)
+    const [items, setItems] = React.useState([])
     
     React.useEffect(()=>{
         //sendAPIRequest() executes twice when called
@@ -14,15 +15,49 @@ export default function(){
         //works haha
         //await only works inside async functions
         async function sendAPIRequest(){
+            //helper
+            function shuffleChoices(correct, incorrect){
+                //merges the two arrays
+                const choicesArray = [correct, ...incorrect]
+                //shuffles choices
+                choicesArray.sort((a, b) => 0.5 - Math.random());
+                return choicesArray
+            }
+            function processData(exp){
+                const itemlist = []
+                for (let categ in exp){
+                    const item = exp[categ]
+                    itemlist.push({
+                        id: nanoid(),
+                        question: item.question,
+                        correctAnswer: item.correct_answer,
+                        choices: shuffleChoices(item.correct_answer, item.incorrect_answers)
+                    })
+                }
+                return itemlist
+            }
+
+
             const response = await fetch('https://opentdb.com/api.php?amount=3',
             { mode: "cors" });
             const trivias = await response.json()
 
-            setQuestions(trivias.results)
+            const exp = trivias.results
+            console.log(exp)
             //I still don't know exactly why, but storing the array using hooks works
             //maybe because after the whole page refreshes, questions variables are now semi-permanently changed
+
+            
+            setItems(() => processData(exp))
         }
+
         sendAPIRequest()
+
+
+        
+    
+        
+        
 ;        
     },[])
 
@@ -36,32 +71,10 @@ export default function(){
     }
     
 
-    function shuffleChoices(correct, incorrect){
-        //merges the two arrays
-        const choicesArray = [correct, ...incorrect]
-        //shuffles choices
-        choicesArray.sort((a, b) => 0.5 - Math.random());
-        return choicesArray
-    }
-
-    function cycleQuestions(list){
-        const itemlist = []
-        for (let categ in list){
-            const item = list[categ]
-            itemlist.push({
-                id: nanoid(),
-                question: item.question,
-                correctAnswer: item.correct_answer,
-                choices: shuffleChoices(item.correct_answer, item.incorrect_answers)
-            })
-        }
-        return itemlist
-    }
     
     
 
 
-    const items = cycleQuestions(questions)
 
     const itemsToTag = items.map(item => (
         <Questions 
