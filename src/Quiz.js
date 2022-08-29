c1061089bdf7e9b711b7f8a526b83a3b81cbe70a
Raphael Quinones <wapquinones28@gmail.com>
2022-08-29 21:11:14 +0800

    play again feature with score reset

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,55 +11,55 @@ export default function(){
     const [score, setScore] = React.useState(0)
     
     React.useEffect(()=>{
-        //sendAPIRequest() executes twice when called
-        //So I utilized useEffect to just call it once
-        //works haha
-        //await only works inside async functions
-        async function sendAPIRequest(){
-            //helper
-            function shuffleChoices(correct, incorrect){
-                //merges the two arrays
-                const choicesArray = [correct, ...incorrect]
-                //shuffles choices
-                choicesArray.sort((a, b) => 0.5 - Math.random());
-                return choicesArray
-            }
-            function processData(exp){
-                const itemlist = []
-                for (let categ in exp){
-                    const item = exp[categ]
-                    itemlist.push({
-                        id: nanoid(),
-                        question: item.question,
-                        correctAnswer: item.correct_answer,
-                        choices: shuffleChoices(item.correct_answer, item.incorrect_answers)
-                    })
+        //refreshes the game when player hits play agian
+        if (playing === true){
+            //reset score to zero when resets
+            setScore(prevScore => 0)
+            //sendAPIRequest() executes twice when called
+            //So I utilized useEffect to just call it once
+            //works haha
+            //await only works inside async functions
+            async function sendAPIRequest(){
+                //helper
+                function shuffleChoices(correct, incorrect){
+                    //merges the two arrays
+                    const choicesArray = [correct, ...incorrect]
+                    //shuffles choices
+                    choicesArray.sort((a, b) => 0.5 - Math.random());
+                    return choicesArray
                 }
-                return itemlist
+                function processData(exp){
+                    const itemlist = []
+                    for (let categ in exp){
+                        const item = exp[categ]
+                        itemlist.push({
+                            id: nanoid(),
+                            question: item.question,
+                            correctAnswer: item.correct_answer,
+                            choices: shuffleChoices(item.correct_answer, item.incorrect_answers)
+                        })
+                    }
+                    return itemlist
+                }
+
+
+                const response = await fetch('https://opentdb.com/api.php?amount=3',
+                { mode: "cors" });
+                const trivias = await response.json()
+
+                const exp = trivias.results
+                //I still don't know exactly why, but storing the array using hooks works
+                //maybe because after the whole page refreshes, questions variables are now semi-permanently changed
+
+                
+                setItems(() => processData(exp))
             }
 
+            sendAPIRequest()
+    }
 
-            const response = await fetch('https://opentdb.com/api.php?amount=3',
-            { mode: "cors" });
-            const trivias = await response.json()
-
-            const exp = trivias.results
-            //I still don't know exactly why, but storing the array using hooks works
-            //maybe because after the whole page refreshes, questions variables are now semi-permanently changed
-
-            
-            setItems(() => processData(exp))
-        }
-
-        sendAPIRequest()
-
-
-        
-    
-        
-        
-;        
-    },[])
+     
+    },[playing])
 
    
     function callPlay(){
@@ -78,7 +78,6 @@ export default function(){
             setScore(prevState => prevState + 1)
         }
     }
-    console.log(score)
     
 
 
@@ -103,7 +102,9 @@ export default function(){
         <button className = "check-answers" onClick = {changePlay}>
             {callPlay() ? "Check Answers" : "Play Again"}
         </button>
-        {playing ? "" : score}
+        <div className = "score">       
+            {playing ? "" : score}
+        </div>
         </div>
         
     )
